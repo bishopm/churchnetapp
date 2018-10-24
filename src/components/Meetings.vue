6a7b347a6344252206162a7a0ead9411d5657b94
Michael Bishop <michael@goodworx.co.za>
2018-10-24 07:47:32 +0200

    Meetings

--- src/components/Meetings.vue
+++ src/components/Meetings.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
     <q-list class="no-border">
-      <circuitfilter class="q-mx-md" @altered="searchdb" :showme="showme()"></circuitfilter>
+      <p class="q-mt-md caption text-center">Circuit meetings</p>
+      <circuitselect class="q-mx-md" @altered="searchdb" :perms="['edit','admin']" showme="1"></circuitselect>
       <q-item v-if="meetings" v-for="meeting in meetings" :key="meeting.id" :to="'/meetings/' + meeting.id">
-        {{meeting}}
+        <q-item-main>{{meeting.description}}&nbsp;<small>({{meeting.society.society}})</small></q-item-main>
+        <q-item-side>
+          <small>{{formatme(meeting.meetingdatetime)}}</small>
+        </q-item-side>
       </q-item>
     </q-list>
     <q-btn round color="positive" @click="addMeeting" class="fixed" icon="add" style="right: 4px; top: 72px" />
@@ -11,7 +15,7 @@
 </template>
 
 <script>
-import circuitfilter from './Circuitfilter'
+import circuitselect from './Circuitselect'
 export default {
   data () {
     return {
@@ -20,7 +24,7 @@ export default {
     }
   },
   components: {
-    'circuitfilter': circuitfilter
+    'circuitselect': circuitselect
   },
   methods: {
     addMeeting () {
@@ -29,23 +33,28 @@ export default {
     showme () {
       return this.$store.state.user.circuits.keys.length
     },
+    formatme (datein) {
+      var fin = new Date(datein * 1000).toISOString()
+      return fin.substring(0, 10) + ' [' + fin.substring(11, 16) + ']'
+    },
     searchdb () {
-      if (this.$store.state.user.circuits.keys) {
-        this.$axios.defaults.headers.common['Authorization'] = 'Bearer ' + this.$store.state.token
-        this.$axios.post(process.env.API + '/meetings/search',
-          {
-            circuits: this.$store.state.circuitfilter
-          })
-          .then(response => {
-            this.meetings = response.data
-            this.$q.loading.hide()
-          })
-          .catch(function (error) {
-            console.log(error)
-            this.$q.loading.hide()
-          })
-      }
+      this.$axios.defaults.headers.common['Authorization'] = 'Bearer ' + this.$store.state.token
+      this.$axios.post(process.env.API + '/meetings/search',
+        {
+          circuit: this.$store.state.select
+        })
+        .then(response => {
+          this.meetings = response.data
+          this.$q.loading.hide()
+        })
+        .catch(function (error) {
+          console.log(error)
+          this.$q.loading.hide()
+        })
     }
+  },
+  mounted () {
+    this.searchdb()
   }
 }
 </script>
