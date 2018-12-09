c043158bb938b85a33d27b3c95abb47a6861ae4f
Michael Bishop <michael@goodworx.co.za>
2018-12-09 10:53:50 +0200

    Midweeks

---
+++ src/components/Midweek.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <q-list class="no-border">
+      <p class="q-mt-md caption text-center">Midweek services</p>
+      <circuitselect class="q-mx-md" @altered="searchdb" :perms="['edit','admin']" showme="1"></circuitselect>
+      <q-item v-if="services" v-for="service in services" :key="service.id" :to="'/midweek/' + service.circuit_id + '/edit/' + service.id">
+        <q-item-main>{{service.description}}</q-item-main>
+        <q-item-side>
+          <small>{{formatme(service.servicedate)}}</small>
+        </q-item-side>
+      </q-item>
+    </q-list>
+    <q-page-sticky expand position="top-right" :offset="[32, 32]">
+      <q-btn round color="primary" @click="addMidweek" class="fixed" icon="fas fa-plus"/>
+    </q-page-sticky>
+  </div>
+</template>
+
+<script>
+import circuitselect from './Circuitselect'
+export default {
+  data () {
+    return {
+      services: [],
+      search: ''
+    }
+  },
+  components: {
+    'circuitselect': circuitselect
+  },
+  methods: {
+    addMidweek () {
+      this.$router.push({name: 'midweekform', params: { action: 'add', circuit: this.$store.state.select }})
+    },
+    showme () {
+      return this.$store.state.user.circuits.keys.length
+    },
+    formatme (datein) {
+      var fin = new Date(datein * 1000).toISOString()
+      return fin.substring(0, 10)
+    },
+    searchdb () {
+      this.$axios.defaults.headers.common['Authorization'] = 'Bearer ' + this.$store.state.token
+      this.$axios.post(process.env.API + '/weekdays/search',
+        {
+          circuit: this.$store.state.select
+        })
+        .then(response => {
+          this.services = response.data
+          this.$q.loading.hide()
+        })
+        .catch(function (error) {
+          console.log(error)
+          this.$q.loading.hide()
+        })
+    }
+  },
+  mounted () {
+    this.searchdb()
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/forms/Midweek.vue
@@ -0,0 +1,142 @@
+<template>
+  <div class="layout-padding">
+    <p class="caption text-center" v-if="circuit">{{circuit.circuit}}</p>
+    <div v-if="$route.params.action" class="q-mx-md q-mt-md text-center caption">
+      {{title}} a midweek service
+    </div>
+    <div class="q-mx-md">
+      <q-field :error="$v.form.description.$error" error-label="Enter a description of the service">
+        <q-input float-label="Service" v-model="form.description" @blur="$v.form.description.$touch()" :error="$v.form.description.$error" />
+      </q-field>
+    </div>
+    <div class="q-ma-md">
+      <q-field :error="$v.form.servicedate.$error" error-label="The date field is required">
+        <q-datetime float-label="Service date" clearable format="YYYY-MM-DD" format24h format-model="string" v-model="form.meetingdatetime" type="datetime" @blur="$v.form.servicedate.$touch()" :error="$v.form.servicedate.$error" />
+      </q-field>
+    </div>
+    <div class="q-ma-lg text-center">
+      <q-btn @click="submit()" color="primary">OK</q-btn>
+      <q-btn class="q-ml-md" @click="$router.go(-1)" color="secondary">Cancel</q-btn>
+      <q-btn class="q-ml-md" @click="deleteme" color="tertiary">Delete</q-btn>
+    </div>
+  </div>
+</template>
+
+<script>
+import { required } from 'vuelidate/lib/validators'
+import { format } from 'quasar'
+const { capitalize } = format
+export default {
+  data () {
+    return {
+      title: capitalize(this.$route.params.action),
+      circuit: this.$route.params.circuit,
+      circuits: [],
+      societyOptions: [],
+      id: '',
+      form: {
+        description: '',
+        servicedate: ''
+      }
+    }
+  },
+  validations: {
+    form: {
+      description: { required },
+      servicedate: { required }
+    }
+  },
+  mounted () {
+    this.circuits.push(this.circuit)
+    this.$axios.defaults.headers.common['Authorization'] = 'Bearer ' + this.$store.state.token
+    this.$axios.post(process.env.API + '/societies/search',
+      {
+        search: '',
+        circuits: this.circuits
+      })
+      .then((response) => {
+        for (var skey in response.data) {
+          var newitem = {
+            label: response.data[skey].society,
+            value: response.data[skey].id
+          }
+          this.societyOptions.push(newitem)
+        }
+        if (this.$route.params.action === 'edit') {
+          this.$axios.defaults.headers.common['Authorization'] = 'Bearer ' + this.$store.state.token
+          this.$axios.get(process.env.API + '/meetings/' + this.$route.params.id)
+            .then((response) => {
+              this.form.description = response.data.description
+              this.form.society_id = response.data.society_id
+              this.form.meetingdatetime = response.data.datestr
+            })
+            .catch(function (error) {
+              console.log(error)
+            })
+        }
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+  },
+  methods: {
+    deleteme () {
+      this.$axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('CHURCHNET_Token')
+      this.$axios.delete(process.env.API + '/meetings/' + this.$route.params.id)
+        .then(response => {
+          this.$router.go(-1)
+        })
+        .catch(function (error) {
+          this.error = error
+        })
+    },
+    submit () {
+      this.$v.form.$touch()
+      if (this.$v.form.$error) {
+        this.$q.notify('Please check for errors!')
+      } else {
+        if (this.$route.params.action === 'add') {
+          this.$axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('CHURCHNET_Token')
+          this.$axios.post(process.env.API + '/meetings',
+            {
+              society_id: this.form.society_id,
+              circuit_id: this.circuit,
+              description: this.form.description,
+              meetingdatetime: this.form.meetingdatetime
+            })
+            .then(response => {
+              console.log(response.data)
+              this.$router.go(-1)
+            })
+            .catch(function (error) {
+              this.error = error
+            })
+        } else {
+          this.$axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('CHURCHNET_Token')
+          this.$axios.post(process.env.API + '/meetings/' + this.$route.params.id + '/update',
+            {
+              society_id: this.form.society_id,
+              circuit_id: this.circuit,
+              description: this.form.description,
+              meetingdatetime: this.form.meetingdatetime
+            })
+            .then(response => {
+              console.log(response.data)
+              this.$router.go(-1)
+            })
+            .catch(function (error) {
+              this.error = error
+            })
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style>
+  a {
+    text-decoration: none;
+    color:white;
+  }
+</style>
