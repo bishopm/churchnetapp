fa85123047b49d81376a5a32cb01ad8d25783e1a
Michael Bishop <michael@goodworx.co.za>
2019-01-08 10:50:19 +0200

    Tweak

--- src/components/Households.vue
+++ src/components/Households.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="layout-padding">
     <p class="caption" v-if="message">{{$message}}</p>
-    <q-list class="no-border">
+    <q-list v-if="households" class="no-border">
       <p class="caption text-center">All households</p>
       <societyfilter @altered="searchdb" initial="all" :showme="showme()"></societyfilter>
       <q-search ref="search" class="q-my-md" @input="searchdb" v-model="search" placeholder="search by addressee" />
-      <q-item v-if="households" v-for="household in households" :key="household.id" :to="'/households/' + household.id">
+      <q-item v-for="household in households" :key="household.id" :to="'/households/' + household.id">
         {{household.addressee}}
       </q-item>
     </q-list>
