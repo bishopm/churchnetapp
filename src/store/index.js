6dc1bd31a6ecc2213269b64a59445ce2bc94aaa5
Michael Bishop <michael@goodworx.co.za>
2019-01-29 07:25:00 +0200

    Tweaks

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,6 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
-
-/*
- * If not building with SSR mode, you can
- * directly export the Store instantiation
- */
-
 export default function (/* { ssrContext } */) {
   const store = new Vuex.Store({
     state: {
