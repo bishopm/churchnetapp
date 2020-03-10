3e3c4730ead001d14df41aba77a0a103c29ef61a
Michael Bishop <michael@goodworx.co.za>
2020-03-10 13:53:47 +0200

    Extra readings

---
+++ src/components/Reading.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h3>Add a reading</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  mounted () {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Readings.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="q-ma-md">
+    <h4 class="text-center">Choose a {{$store.state.user.denomination.local}}</h4>
+    <q-list v-if="$store.state.user" class="no-border q-my-md">
+      <q-item v-for="society in $store.state.user.societies.full" :key="society.id" :to="'/statistics/' + society.id + '/' + (new Date()).getFullYear()">
+        {{society.society}}
+      </q-item>
+    </q-list>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+</style>
